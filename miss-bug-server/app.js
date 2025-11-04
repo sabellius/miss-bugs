1338165b54411959350ea50e346c9902df71d98f
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-11-04 14:23:10 +0200

    enable cors to allow communication between server and client

--- miss-bug-server/app.js
+++ miss-bug-server/app.js
@@ -1,7 +1,16 @@
-import express from "express";
-import routes from "./routes/index.js";
+import express from 'express';
+import cors from 'cors';
+
+import routes from './routes/index.js';
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use("/", routes);
-app.listen(3030, () => console.log("Server ready at port 3030"));
+app.use('/', routes);
+app.listen(3030, () => console.log('Server ready at port 3030'));
