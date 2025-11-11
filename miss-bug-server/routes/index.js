f8c270d5c54dbb4b7d476ca2cf880214d932caa0
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-11-11 16:06:14 +0200

    feat: add full crud for users

--- miss-bug-server/routes/index.js
+++ miss-bug-server/routes/index.js
@@ -1,8 +1,10 @@
-import express from "express";
-import bugsRouter from "./bugs.js";
+import express from 'express';
+import bugsRouter from './bugs.js';
+import usersRouter from './users.js';
 
 const router = express.Router();
 
-router.use("/bugs", bugsRouter);
+router.use('/bugs', bugsRouter);
+router.use('/users', usersRouter);
 
 export default router;
